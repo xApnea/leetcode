26a057421e705a7dcf28321d3702cbf6e9478791
xApnea <jcbruno150@gmail.com>
2020-08-12 21:49:39 -0400

    nth Fibonacci w recursion, dunamic programming

--- fibonacciNumber.js
+++ fibonacciNumber.js
@@ -3,5 +3,26 @@
  * @return {number}
  */
 var fib = function(N) {
+  var result;
+  var memo = [0, 1, 1];
 
+  var findfib = function(n) {
+    if (memo[n] !== undefined) {
+      return memo[n];
+    }
+    if (n === 1 || n === 2) {
+      return 1;
+    } else {
+      var first = findfib(n - 1);
+      var second = findfib(n - 2)
+      result =  first + second;
+      memo[n] = result;
+    }
+    return result;
+  }
+
+  result = findfib(N);
+  return result;
 };
+
+console.log(fib(5));
