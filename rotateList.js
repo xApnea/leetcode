8817cbf81e0fc615c2586116463b1277b0abe632
xApnea <jcbruno150@gmail.com>
2020-08-24 18:17:40 -0400

    Solved

--- rotateList.js
+++ rotateList.js
@@ -12,31 +12,44 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
+  if (!head || !head.next) return head;
+
   var rotated;
   var tail = head;
+  var length = 1;
 
   while (tail.next) {
     tail = tail.next;
+    length++;
   }
-  tail.next = head;
 
-  for (var i = 1; i <= k; i++) {
-    if (i === k) {
-      tail.next = null;
-      return head;
-    }
-    head = head.next;
-    tail = tail.next;
+  if (k %= length) {
+    tail.next = head;
+    for (var i = 0; i < length - k; i++) {
+      tail = tail.next
+    };
+    head = tail.next;
+    tail.next = null;
   }
+
+  return head;
 };
 
-var list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))));
-var result = rotateRight(list, 2);
-var values = [2, 3, 4, 5, 1];
-var index = 0;
+// var list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))));
+// var result = rotateRight(list, 2);
+// var values = [4, 5, 1, 2, 3];
+// var index = 0;
 
-while (result.next) {
-  console.assert(result.val === values[index]);
+// while (result.next) {
+//   console.log(result.val);
+//   console.assert(result.val === values[index]);
+//   result = result.next;
+//   index++;
+// }
+
+var list = ListNode(0, ListNode(1, ListNode(2)));
+var result = rotateRight(list, 4);
+for (i = 0; i < 3; i++) {
+  console.log(result.val);
   result = result.next;
-  index++;
 }
