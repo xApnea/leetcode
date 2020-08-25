e342fd27dff5889545511fe5b53734772bc501f1
xApnea <jcbruno150@gmail.com>
2020-08-25 13:39:50 -0400

    init

---
+++ turnstile.js
@@ -0,0 +1,27 @@
+// Inputs:
+// num of customers
+// array of times that each customer arrived at
+//  The above arrays are ordered by time arrival
+// array of directions: 1 = wants to exit, 0 = wants to enter
+
+// Output:
+// array of the times that each customer at index i passed through the turnstile
+
+// constraints:
+// 1 <= numOfCustomers <= 10^5
+// times are in ascending order from 0 to 10^9 secs
+// direction will only be 0 or 1
+
+/**
+ * @return {number} numOfCustomers
+ * @param {number[]} times
+ * @param {number[]} directions
+ *
+ */
+
+ var amazonTurnstile = function(numOfCustomers, times, directions) {
+   var results = [];
+
+
+   return results;
+ }
