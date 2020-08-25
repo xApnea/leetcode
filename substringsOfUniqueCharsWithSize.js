87be369f30a9050d21d7c16c48cdf85e706d6eac
xApnea <jcbruno150@gmail.com>
2020-08-25 13:18:26 -0400

    Solved using a sliding window, linear time

--- substringsOfUniqueCharsWithSize.js
+++ substringsOfUniqueCharsWithSize.js
@@ -12,5 +12,38 @@
  */
 
 var findSubstrings = function (str, k) {
+  var results = [];
+  var substrings = {};
 
+  if (str.length < k) { return results}
+
+  for (var i = 0; i <= str.length - k; i++) {
+    var sum = i + k;
+    var charsUsed = {};
+    var flag = true;
+    var window = [];
+    for (var j = i; j < sum; j++) {
+
+      if (!charsUsed[str[j]]) {
+        window.push(str[j]);
+        charsUsed[str[j]] = true;
+      } else {
+        flag = false;
+        break;
+      }
+    }
+    var substring = window.join('');
+    if (flag && !substrings[substring]) {
+      results.push(substring);
+      substrings[substring] = true;
+    }
+  }
+  return results;
 }
+
+var result = findSubstrings("abacab", 3);
+console.log(result);
+result = findSubstrings("abcabc", 3);
+console.log(result);
+result = findSubstrings("awaglknagawunagwkwagl", 4);
+console.log(result);
