5f077f9c5674ccf810d9f21066fb54364699f010
xApnea <jcbruno150@gmail.com>
2020-08-25 12:38:58 -0400

    init

---
+++ substringsOfUniqueCharsWithSize.js
@@ -0,0 +1,16 @@
+// Given a string s and an int k, return all unique substrings of s of size k with k distinct characters.
+
+// Input: s = "abcabc", k = 3
+// Output: ["abc", "bca", "cab"]
+
+// Input: s = "abacab", k = 3
+// Output: ["bac", "cab"]
+
+/**
+ * @param {string} str
+ * @param {number} k
+ */
+
+var findSubstrings = function (str, k) {
+
+}
