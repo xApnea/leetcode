b998b40b67be3ca03fadb3a7e3d913d73ac16e8e
xApnea <jcbruno150@gmail.com>
2020-08-27 15:45:07 -0400

    Clarified inputs and solution format

--- largestItemAssociation.js
+++ largestItemAssociation.js
@@ -1,14 +1,16 @@
 // O: Largest Item Association [item4, item5, item6] as an Array
-// I: an Array of Tuples that inlude of pairs of strings
+// I: an Array of PairStrings that inlude of pairs of strings
 // C:
-// E: there can be tuples with only 1 string, this counts as a size of 1.
-//    There cannot be null tuples, all will at least have one
+// E: there can be PairStrings with only 1 string, this counts as a size of 1.
+//    There cannot be null PairStrings, all will at least have one
 
 var PairString = function(first, second) {
     this.first = first;
     this.second = second;
 }
 
-var largestItemAssociation = function(PairString, itemAssociation) {
+var largestItemAssociation = function(itemAssociation) {
 
 }
+
+var itemAssociation = [PairString('item1', 'item2'), PairString('item3', 'item4'), PairString('item4', 'item5')];
