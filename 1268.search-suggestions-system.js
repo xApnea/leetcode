ef44561e9300ade3ea2f99d873a1a72ca83600ea
xApnea <jcbruno150@gmail.com>
2020-08-28 18:54:56 -0400

    Solved with Brute Force

--- 1268.search-suggestions-system.js
+++ 1268.search-suggestions-system.js
@@ -11,7 +11,30 @@
  * @return {string[][]}
  */
 var suggestedProducts = function(products, searchWord) {
-    
+  // sort products lexciographically
+  products = products.sort((a, b) => {
+    return a.localeCompare(b);
+  })
+
+  var suggested = [];
+  // for every character in searchWord
+  var searchTerm = '';
+  for (character of searchWord) {
+    searchTerm += character;
+    var hits = [];
+    for (product of products) {
+      const prefix = product.slice(0, searchTerm.length);
+      if (prefix === searchTerm) hits.push(product);
+      if (hits.length === 3) break;
+    }
+    suggested.push(hits);
+  }
+
+  return suggested;
 };
+
+let results = suggestedProducts(["mobile","mouse","moneypot","monitor","mousepad"], "mouse");
+console.log(results);
+
 // @lc code=end
 
