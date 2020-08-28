b71172085094426cf6e1725b157afdb8d4722f5e
xApnea <jcbruno150@gmail.com>
2020-08-28 19:23:17 -0400

    init

---
+++ 937.reorder-data-in-log-files.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=937 lang=javascript
+ *
+ * [937] Reorder Data in Log Files
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} logs
+ * @return {string[]}
+ */
+var reorderLogFiles = function(logs) {
+
+};
+
+var logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"];
+var results = reorderLogFiles(logs);
+
+
+// @lc code=end
+
