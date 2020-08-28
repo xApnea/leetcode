153fd26db8d9e6f3d91c49f7e3123c4f4b95ceaf
xApnea <jcbruno150@gmail.com>
2020-08-28 18:17:45 -0400

    init

---
+++ 994.rotting-oranges.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=994 lang=javascript
+ *
+ * [994] Rotting Oranges
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+
+};
+
+var result = orangesRotting([[2,1,1],[1,1,0],[0,1,1]]);
+console.assert(result === 4, `Expected 4, received ${result}`);
+
+// [[2,1,1],
+//  [1,1,0],
+//  [0,1,1]]
+
+
+
+
+// @lc code=end
+
