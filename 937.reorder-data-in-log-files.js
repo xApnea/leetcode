5dda973de05af6790878cd814a8305f3b6813ca8
xApnea <jcbruno150@gmail.com>
2020-08-28 19:23:33 -0400

    Solved using iterative and default sort

--- 937.reorder-data-in-log-files.js
+++ 937.reorder-data-in-log-files.js
@@ -10,7 +10,36 @@
  * @return {string[]}
  */
 var reorderLogFiles = function(logs) {
+  var digitLogs = [];
+  var letterLogs = [];
 
+  // iterate through the logs
+  for (log of logs) {
+    const prefix = log.split(' ', 1);
+    const id = prefix[0].slice(0, 3);
+    if (id === 'dig') digitLogs.push(log);
+    else if (id === 'let') letterLogs.push(log);
+  }
+
+  var sortedLetterLogs = letterLogs.sort((a, b) => {
+    var aSplit = a.split(' ');
+    var bSplit = b.split(' ');
+
+    var aPrefix = aSplit.shift();;
+    var bPrefix = bSplit.shift();;
+
+    var aString = aSplit.join(' ');
+    var bString = bSplit.join(' ');
+
+    var result = aString.localeCompare(bString);
+    if (result === 0) {
+      return aPrefix.localeCompare(bPrefix);
+    } else {
+      return result;
+    }
+  })
+
+  return sortedLetterLogs.concat(digitLogs);
 };
 
 var logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"];
