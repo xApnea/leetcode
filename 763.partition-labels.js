a460be4c06f238a1dae946062e9e65898e7c429d
xApnea <jcbruno150@gmail.com>
2020-08-28 19:55:02 -0400

    init

---
+++ 763.partition-labels.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=763 lang=javascript
+ *
+ * [763] Partition Labels
+ */
+
+// @lc code=start
+/**
+ * @param {string} S
+ * @return {number[]}
+ */
+var partitionLabels = function(S) {
+    
+};
+// @lc code=end
+
