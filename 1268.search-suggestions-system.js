7456e49c20184d593956a67c1ef5dc9c90e3e03c
xApnea <jcbruno150@gmail.com>
2020-08-28 18:40:09 -0400

    init

---
+++ 1268.search-suggestions-system.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1268 lang=javascript
+ *
+ * [1268] Search Suggestions System
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+var suggestedProducts = function(products, searchWord) {
+    
+};
+// @lc code=end
+
