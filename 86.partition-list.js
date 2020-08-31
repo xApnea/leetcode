6042aa1967bdd46f90ebcb1d08f411ba42eb92e9
xApnea <jcbruno150@gmail.com>
2020-08-31 13:16:16 -0400

    Solved using double traversal of linked list

--- 86.partition-list.js
+++ 86.partition-list.js
@@ -5,20 +5,68 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
 /**
  * @param {ListNode} head
  * @param {number} x
  * @return {ListNode}
  */
 var partition = function(head, x) {
-    
+  // iterate through list
+  // find x
+  var node = head;
+  if (node === null) return null;
+
+  while (node !== null) {
+    if (node.val === x) break;
+    else if (node.next === null) return head;
+    else node = node.next;
+  }
+  // once x is hit, start testing if node.next.val is < x
+  var prevNode = node;
+  node = node.next;
+  while (node) {
+    if (node.val <= x) {
+      //  if yes. remove node, and reconnect
+      prevNode.next = node.next;
+      // node is our 'temp' node
+      var node2 = head;
+      // iterate list a second time, testing where the new value falls. Find where it is < next.value, and insert
+      while (node2.next) {
+        if (node.val >= node2.val && node.val < node2.next.val) {
+          var tail = node2.next;
+          node2.next = node;
+          node.next = tail;
+          break;
+        } else {
+          node2 = node2.next;
+        }
+      }
+      node = prevNode.next;
+    } else {
+      prevNode = node;
+      node = node.next;
+
+    }
+  }
+  return head;
 };
+
+// var list = new ListNode(1, new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(5, new ListNode(2))))));
+// 1->4->3->2->5->2
+
+// partition(list, 3);
+
+// var list = new ListNode(1);
+// partition(list, 0);
+
+// [1,1], 1
+
+var list = new ListNode(1, new ListNode(1));
+partition(list, 1);
+
 // @lc code=end
 
