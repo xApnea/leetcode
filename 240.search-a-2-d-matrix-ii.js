9ead88a8d0491f33ec4ab31748b8d07e455251ed
xApnea <jcbruno150@gmail.com>
2020-09-03 17:19:13 -0400

    Solved using brute force approach

--- 240.search-a-2-d-matrix-ii.js
+++ 240.search-a-2-d-matrix-ii.js
@@ -11,13 +11,24 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
+  if (!matrix[0]) return false;
+
   const rowLength = matrix[0].length;
   const colLength = matrix.length;
 
   // If target is larger than the biggest element, return false immediately
-  if (matrix[colLength][rowLength] > target) return false;
+  if (matrix[colLength - 1][rowLength - 1] < target) return false;
 
+  for (var i = 0; i < colLength; i++) {
+    for (var j = 0; j < rowLength; j++) {
+      if (matrix[i][j] === target) {
+        return true;
+      }
+    }
+  }
 
+  return false;
 };
 // @lc code=end
 
+searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5);
