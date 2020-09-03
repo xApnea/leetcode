2a9c997a1dcc4af22349593f37cf0ae5e643ac92
xApnea <jcbruno150@gmail.com>
2020-09-03 13:29:17 -0400

    init

---
+++ 1.two-sum.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+};
+// @lc code=end
+
+var input = [2,7,11,15];
+var result = twoSum(input, 9);
+console.log(result);
